4e3ffe91fca6af26353af0fa5911508478da7a31
wang1212 <mrwang1212@126.com>
2022-01-05 23:43:22 +0800

    build(templates): `image-webpack-loader` is not enabled by default :heavy_minus_sign:

--- templates/app/config/webpack.config.js
+++ templates/app/config/webpack.config.js
@@ -197,29 +197,30 @@ module.exports = ({ NODE_ENV, SRC_DIR, BUILD_DIR, isEnvDevelopment = NODE_ENV ==
         //   },
         // },
         use: [
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              disable: isEnvDevelopment,
-              mozjpeg: {
-                progressive: true,
-                quality: 65,
-              },
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: [0.65, 0.9],
-                speed: 4,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              webp: {
-                quality: 75,
-              },
-            },
-          },
+          // install https://github.com/tcoopman/image-webpack-loader#install
+          // {
+          //   loader: 'image-webpack-loader',
+          //   options: {
+          //     disable: isEnvDevelopment,
+          //     mozjpeg: {
+          //       progressive: true,
+          //       quality: 65,
+          //     },
+          //     optipng: {
+          //       enabled: false,
+          //     },
+          //     pngquant: {
+          //       quality: [0.65, 0.9],
+          //       speed: 4,
+          //     },
+          //     gifsicle: {
+          //       interlaced: false,
+          //     },
+          //     webp: {
+          //       quality: 75,
+          //     },
+          //   },
+          // },
         ],
       },
     ],
