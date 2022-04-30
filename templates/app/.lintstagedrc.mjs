8b8841bf9180046c56c61bb3ddc5e21b11213351
wang1212 <mrwang1212@126.com>
2022-04-30 18:22:05 +0800

    chore(app): remove ext .jsx,.tsx for eslint lint :wastebasket:

--- templates/app/.lintstagedrc.mjs
+++ templates/app/.lintstagedrc.mjs
@@ -1,6 +1,6 @@
 // see docs: https://github.com/okonet/lint-staged#configuration
 
 export default {
-  '*.{js,jsx,ts,tsx}': 'npm run lint-js',
+  '*.{js,ts}': 'npm run lint-js',
   '*.{css,scss,sass}': 'npm run lint-css',
 };
